4804b300d88a22795dad12b9396476f2d2e94519
Thatguyjs <that.program@gmail.com>
2022-01-22 23:54:03 -0700

    Update Calc-JS, add range-based error display

--- popup/main.mjs
+++ popup/main.mjs
@@ -1,8 +1,9 @@
-// Copyright (c) 2021 Thatguyjs All Rights Reserved.
+// Copyright (c) 2022 Thatguyjs All Rights Reserved.
 
 import addons from "./addons.mjs";
 import CalcHistory from "./history.mjs";
 import Calculator from "/Calc-JS/src/include.mjs";
+import ErrorHandler from "./error.mjs";
 
 
 const Main = {
@@ -18,7 +19,8 @@ const Main = {
 	init: function() {
 		input.focus();
 
-		input.addEventListener('focusout', () => {
+		input.addEventListener('focusout', (ev) => {
+			// if(!ev.relatedTarget)
 			input.focus();
 		});
 
@@ -73,6 +75,7 @@ const Main = {
 		if(!input.value.length) return;
 
 		const results = Calculator.eval(input.value, addons);
+		let had_error = false;
 		let res_data = [];
 
 		for(let r in results) {
@@ -81,12 +84,19 @@ const Main = {
 			if(Array.isArray(val)) val = `[${val.join(', ')}]`;
 			else val = val?.toString();
 
-			res_data.push(results[r].error.has_error() ? results[r].error.message : val);
+			if(results[r].error.has_error()) {
+				had_error = true;
+				break;
+			}
+			else res_data.push(val);
 		}
 
 		let res_string = res_data.join(', ');
 		CalcHistory.store(input.value, res_string);
-		input.value = res_string;
+		ErrorHandler.clear();
+
+		if(!had_error) input.value = res_string;
+		else ErrorHandler.display_errors(input.value, results);
 
 		this.input_state.current = 'live';
 		this.input_state.history = 0;
--- tests/index.mjs
+++ tests/index.mjs
@@ -3,7 +3,15 @@ import { expect_eq, finish } from "./lib.mjs";
 import addons from "../popup/addons.mjs";
 import Calculator from "../Calc-JS/src/include.mjs";
 
-let test_results = { total: 0, passed: 0, failed: [] };
+
+function error_string(err) {
+	if(err.location.start === null)
+		return `Error: ${err.message}`;
+	else if(err.location.start === err.location.end)
+		return `Error at ${err.location.start}: ${err.message}`;
+	else
+		return `Error from ${err.location.start} to ${err.location.end}: ${err.message}`;
+}
 
 function calculate(input) {
 	let result = Calculator.eval(input, addons);
@@ -15,7 +23,7 @@ function calculate(input) {
 		if(Array.isArray(val)) val = `[${val.join(', ')}]`;
 		else val = val?.toString();
 
-		res_list.push(result[r].error.has_error() ? result[r].error.message : val);
+		res_list.push(result[r].error.has_error() ? error_string(result[r].error) : val);
 	}
 
 	return res_list.join(', ');
@@ -94,16 +102,16 @@ test("gcf(230, 590)", "10");
 test("hex(ff)", "255");
 test("hex(1fc8)", "8136");
 test("hex(0x1fc8)", "8136");
-test("hex(fg)", "Invalid Hex String");
+test("hex(fg)", "Error: Invalid Hex String");
 test("oct(173)", "123");
 test("oct(17710)", "8136");
 test("oct(0o17710)", "8136");
-test("oct(79)", "Invalid Octal String");
+test("oct(79)", "Error: Invalid Octal String");
 test("bin(10)", "2");
 test("bin(111)", "7");
 test("bin(1111111001000)", "8136");
 test("bin(0b1111111001000)", "8136");
-test("bin(2)", "Invalid Binary String");
+test("bin(2)", "Error: Invalid Binary String");
 test("convert(100 meters as feet)", "328.08399", false);
 test("convert(4.5 yd, mi)", "0.0025568133");
 test("convert(48 * 2 kg to lb)", "211.6437950272", false);
@@ -112,9 +120,9 @@ test("convert(360 / 2 deg as rad)", "3.1415926536", false);
 test("convert(1 rad, deg)", "57.2957795131");
 test("convert(600 secs to days)", "0.0069444444", false);
 test("convert(1 - 0.6 yr, mins)", "210239.695152442");
-test("convert()", "Missing Parameters");
-test("convert(2 meters)", "Missing Parameters");
-test("convert(25 programmers to people)", "Unknown Unit", false);
+test("convert()", "Error: Missing Parameters");
+test("convert(2 meters)", "Error: Missing Parameters");
+test("convert(25 programmers to people)", "Error: Unknown Unit", false);
 test("range(0, 5)", "[0, 1, 2, 3, 4, 5]");
 test("range(-3, 3)", "[-3, -2, -1, 0, 1, 2, 3]");
 test("range(3, -1)", "[3, 2, 1, 0, -1]");
@@ -175,14 +183,18 @@ test("floor(4.99)", "4");
 test("ceil(2.01)", "3");
 test("sum(2 + 3 / 2, 4 * 5)", "23.5");
 
-test("(-1)!", "Invalid Operation");
-test("( )", "Invalid Expression");
-test("(", "Invalid Expression");
-test(")", "Invalid Expression");
-test("1, ,2", "Invalid Expression");
-test("a==1", "Invalid Expression");
-test("4 + a", "Unknown Variable");
-test("notafunc(123, 456)", "Unknown Function");
-test("range(1, 1)", "Invalid Range Bounds");
+// Error messages
+test("(-1)!", "Error from 1 to 2: Invalid Operation");
+test("( )", "Error from 0 to 2: Invalid Expression", false);
+test("()", "Error from 0 to 1: Invalid Expression");
+test("(", "Error at 0: Invalid Expression");
+test(")", "Error at 0: Invalid Expression");
+test("1, ,2", "Error from 1 to 3: Invalid Expression", false);
+test("1,,2", "Error from 1 to 2: Invalid Expression");
+test("a==1", "Error from 1 to 2: Invalid Expression");
+test("4 + a", "Error at 4: Unknown Variable", false);
+test("4+a", "Error at 2: Unknown Variable");
+test("notafunc(123, 456)", "Error from 0 to 7: Unknown Function");
+test("range(1, 1)", "Error: Invalid Range Bounds");
 
 finish();
